4fe465687a658f4ac49fc778c21f40673518adea
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-04 14:35:22 +0100

    added read all and create to Game Service

--- src/main/java/com/qa/service/GamesService.java
+++ src/main/java/com/qa/service/GamesService.java
@@ -1,8 +1,11 @@
 package com.qa.service;
 
+import com.qa.domain.Game;
 import com.qa.repo.GamesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GamesService {
@@ -14,6 +17,13 @@ public class GamesService {
         this.repo = repo;
     }
 
+    public List<Game> readGames(){
+        return this.repo.findAll();
+    }
+
+    public Game createGame(Game game){
+        return this.repo.save(game);
+    }
 
 
 }
