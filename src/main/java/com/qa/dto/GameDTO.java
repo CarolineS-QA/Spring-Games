83e8cca3b9fa9fd65f59929c313f0fa72779a8f5
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-05 15:49:41 +0100

    created GameDTO

---
+++ src/main/java/com/qa/dto/GameDTO.java
@@ -0,0 +1,55 @@
+package com.qa.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class GameDTO {
+
+    private Long gameId;
+    private String title;
+    private String description;
+    private Date releaseDate;
+    private BigDecimal price;
+
+    public GameDTO() {
+    }
+
+    public GameDTO(String title, String description, Date releaseDate, BigDecimal price) {
+        this.title = title;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
