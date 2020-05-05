4a5eee5c1a6324396af7930af0fd5e54baa2347a
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-05 11:15:46 +0100

    modified CollectionService with CRUD and added CollectionNotFoundException

--- src/main/java/com/qa/service/CollectionService.java
+++ src/main/java/com/qa/service/CollectionService.java
@@ -1,8 +1,12 @@
 package com.qa.service;
 
+import com.qa.domain.Collection;
+import com.qa.exceptions.CollectionNotFoundException;
 import com.qa.repo.CollectionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CollectionService {
@@ -11,5 +15,31 @@ public class CollectionService {
     @Autowired
     public CollectionService(CollectionRepo repo) {
         this.repo = repo;
+    }
+
+    public List<Collection> readCollections(){
+        return this.repo.findAll();
+    }
+
+    public Collection createCollection(Collection collection){
+        return this.repo.save(collection);
+    }
+
+    public Collection findCollectionById(Long id){
+        return this.repo.findById(id).orElseThrow(CollectionNotFoundException::new);
+    }
+
+    public Collection updateCollection(Long id, Collection collection){
+        Collection update = findCollectionById(id);
+        update.setName(collection.getName());
+        return this.repo.save(update);
+    }
+
+    public boolean deleteCollection(Long id){
+        if(!this.repo.existsById(id)){
+            throw new CollectionNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
     }
 }
