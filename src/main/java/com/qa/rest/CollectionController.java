83b6e1ac6c4bee724848d3de3336151edaa4bafc
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-05 11:23:43 +0100

    added endpoints to CollectionController

--- src/main/java/com/qa/rest/CollectionController.java
+++ src/main/java/com/qa/rest/CollectionController.java
@@ -1,8 +1,12 @@
 package com.qa.rest;
 
+import com.qa.domain.Collection;
+import com.qa.domain.Game;
 import com.qa.service.CollectionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CollectionController {
@@ -11,5 +15,30 @@ public class CollectionController {
     @Autowired
     public CollectionController(CollectionService service) {
         this.service = service;
+    }
+
+    @GetMapping("/getAllCollections")
+    public List<Collection> getAllCollections(){
+        return this.service.readCollections();
+    }
+
+    @PostMapping("/createCollection")
+    public Collection createCollection(@RequestBody Collection collection){
+        return this.service.createCollection(collection);
+    }
+
+    @GetMapping("/getCollectionById/{id}")
+    public Collection getCollectionById(@PathVariable Long id){
+        return this.service.findCollectionById(id);
+    }
+
+    @PutMapping("/updateCollection/{id}")
+    public Collection updateCollection(@PathVariable Long id, @RequestBody Collection collection){
+        return this.service.updateCollection(id, collection);
+    }
+
+    @DeleteMapping("/deleteCollection/{id}")
+    public boolean deleteCollection(@PathVariable Long id){
+        return this.service.deleteCollection(id);
     }
 }
