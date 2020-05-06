24c597d3ca497b5d128f4a773d694e9bf4e8bc2c
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-06 10:07:49 +0100

    modified GamesController to return ResponseEntities and use DTO's

--- src/main/java/com/qa/rest/GamesController.java
+++ src/main/java/com/qa/rest/GamesController.java
@@ -1,8 +1,11 @@
 package com.qa.rest;
 
 import com.qa.domain.Game;
+import com.qa.dto.GameDTO;
 import com.qa.service.GamesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +20,29 @@ public class GamesController {
     }
 
     @GetMapping("/getAllGames")
-    public List<Game> getAllGames(){
-        return this.service.readGames();
+    public ResponseEntity<List<GameDTO>> getAllGames(){
+        return ResponseEntity.ok(this.service.readGames());
     }
 
     @PostMapping("/createGame")
-    public Game createGame(@RequestBody Game game){
-        return this.service.createGame(game);
+    public ResponseEntity<GameDTO> createGame(@RequestBody Game game){
+        return new ResponseEntity<GameDTO>(this.service.createGame(game), HttpStatus.CREATED);
     }
 
     @GetMapping("/getGameById/{id}")
-    public Game getGameById(@PathVariable Long id){
-        return this.service.findGameById(id);
+    public ResponseEntity<GameDTO> getGameById(@PathVariable Long id){
+        return ResponseEntity.ok(this.service.findGameById(id));
     }
 
     @PutMapping("/updateGame/{id}")
-    public Game updateGame(@PathVariable Long id, @RequestBody Game game){
-        return this.service.updateGame(id, game);
+    public ResponseEntity<GameDTO> updateGame(@PathVariable Long id, @RequestBody Game game){
+        return ResponseEntity.ok(this.service.updateGame(id, game));
     }
 
     @DeleteMapping("/deleteGame/{id}")
-    public boolean deleteGame(@PathVariable Long id){
-        return this.service.deleteGame(id);
+    public ResponseEntity<?> deleteGame(@PathVariable Long id){
+        return this.service.deleteGame(id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
     }
 }
